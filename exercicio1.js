0d3b12fd4e05ea8182222dbd3bad918bdcae8183
Rodrigo Reis <rodrigo.br.reis@gmail.com>
2024-09-30 21:59:15 -0300

    Adicionando conteúdo de operadores

---
+++ exercicio1.js
@@ -0,0 +1,27 @@
+let primeiroValor
+let segundoValor
+let terceiroValor
+let quartoValor
+let quintoValor
+let sextoValor
+let resultado
+
+
+primeiroValor = 3
+segundoValor = 4
+terceiroValor = 5
+quartoValor = 2
+quintoValor = 1
+sextoValor = 234
+
+resultado = primeiroValor + segundoValor
+console.log ('primeiro cálculo', resultado) 
+
+resultado = primeiroValor * terceiroValor / quartoValor
+console.log ('segundo cálculo', resultado) 
+
+resultado = (segundoValor - terceiroValor) * - quintoValor
+console.log ('terceiro cálculo', resultado) 
+
+resultado = sextoValor % terceiroValor
+console.log ('quarto cálculo', resultado) 
---
+++ exercicio2.js
@@ -0,0 +1,15 @@
+const a = ( 9 )
+const b = ( 152 ) 
+let resultado
+
+resultado = a === b
+console.log ( 'primeira comparação', resultado )
+
+resultado = a !== b
+console.log ( 'segunda comparação', resultado )
+
+resultado = a > b
+console.log ( 'terceira comparação', resultado )
+
+resultado = a < b
+console.log ( 'quarta comparação', resultado )
